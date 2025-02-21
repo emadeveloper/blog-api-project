18e7d3756ae096e57e907c1e11a95a2d8907c6ca
Emanuel Martinez <yinproduccuiones@gmail.com>
2025-02-21 14:13:25 -0300

    first commit/initial structure

---
+++ src/test/java/com/blog_spring_boot_api/blog_spring_boot_api/BlogSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog_spring_boot_api.blog_spring_boot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
