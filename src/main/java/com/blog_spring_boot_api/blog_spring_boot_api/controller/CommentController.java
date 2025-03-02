0399ed6ab2424a5215b96750e06caa5076d46623
Emanuel Martinez <yinproduccuiones@gmail.com>
2025-03-02 14:21:27 -0300

    feature/get-comments

--- src/main/java/com/blog_spring_boot_api/blog_spring_boot_api/controller/CommentController.java
+++ src/main/java/com/blog_spring_boot_api/blog_spring_boot_api/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.blog_spring_boot_api.blog_spring_boot_api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,11 @@ import com.blog_spring_boot_api.blog_spring_boot_api.services.CommentService;
 public class CommentController {
     @Autowired
     private CommentService commentService;
+
+    @GetMapping("/posts/{postId}/comments")
+    public List<CommentDTO> getCommentsById(@PathVariable(value = "postId") long postId){
+        return commentService.getCommentsByPostId(postId);
+    }
 
     @PostMapping("/posts/{postId}/comments")
     public ResponseEntity <CommentDTO> saveComment(@PathVariable(value = "postId") long postId, @RequestBody CommentDTO commentDTO){
--- src/main/java/com/blog_spring_boot_api/blog_spring_boot_api/repository/CommentRepository.java
+++ src/main/java/com/blog_spring_boot_api/blog_spring_boot_api/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.blog_spring_boot_api.blog_spring_boot_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blog_spring_boot_api.blog_spring_boot_api.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    
+    public List<Comment> findByPostId(long postId);
     
 }
